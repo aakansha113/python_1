9abfd86045b165210dbad9098018b126eb72b418
aakansha113 <155349555+aakansha113@users.noreply.github.com>
2025-03-03 22:48:19 +0530

    string.py

--- python_1/string/string.py
+++ python_1/string/string.py
@@ -17,5 +17,5 @@ str="aakansha hello"
 print(str.endswith("lo"))               # return true if it ends with substr (True)
 print(str.capitalize())                 # capatilize 1 st letter (Aakansha)
 print(str.replace("hello" , "Hujare"))  # replace words or letter from old to new one(Hujare)
-print(str.find("ll"))                   # find the str and write 1 index in 1 occurance(9) 
+print(str.find("ll"))                   # find the str and write 1 index in  occurance(9) 
 print(str.count("a"))                   # count the occurance of substr from whole str (4)
