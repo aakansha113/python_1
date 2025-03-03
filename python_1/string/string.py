fb169800180cea34250bd6551cbc3cb68ebef976
aakansha113 <155349555+aakansha113@users.noreply.github.com>
2025-03-03 22:40:06 +0530

    string.py

--- python_1/string/string.py
+++ python_1/string/string.py
@@ -14,8 +14,8 @@ print(str[-8:-3])     #slicing baackward counting
 print(str[-9:-2])
 
 str="aakansha hello"
-print(str.endswith("lo"))    # return true if it ends with substr (True)
-print(str.capitalize())      # capatilize 1 st letter (Aakansha)
-print(str.replace("hello" , "Hujare"))  #replace words or letter from old to new one(Hujare)
-print(str.find("ll"))     # find the str and write 1 index in 1 occurance(9) 
-print(str.count("a"))       # count the occurance of substr from whole str (4)
+print(str.endswith("lo"))               # return true if it ends with substr (True)
+print(str.capitalize())                 # capatilize 1 st letter (Aakansha)
+print(str.replace("hello" , "Hujare"))  # replace words or letter from old to new one(Hujare)
+print(str.find("ll"))                   # find the str and write 1 index in 1 occurance(9) 
+print(str.count("a"))                   # count the occurance of substr from whole str (4)
