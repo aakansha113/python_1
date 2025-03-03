5ed79d298519cc6dfdbffdd0a8672aeb56813778
aakansha <aakanshahujare@gmail.com>
2025-03-03 17:41:32 +0000

    addd

---
+++ python_1/string/pratice.py
@@ -0,0 +1,11 @@
+# WAP to  input user  first name and print its length
+
+name = input("Enter your first name:") 
+print(len(name))
+
+
+#WAP to find the occurance of $  in string
+
+
+a = "$all the $thing here is given by $all the letter so be  $alert"
+print("here the $ occurs ",(a.count("$")),"times")
