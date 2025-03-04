099b50added968375dd63e3b30996a72aed499d7
aakansha <aakanshahujare@gmail.com>
2025-03-04 07:23:17 +0000

    add

--- python/statement/pratice.py
+++ python/statement/pratice.py
@@ -1,8 +1,50 @@
-#WAP  TO CHECk WHETHER THE GIVEN NUMBER ENTER BY USER IS EVEN OR ODD.
+#Q1-WAP  TO CHECk WHETHER THE GIVEN NUMBER ENTER BY USER IS EVEN OR ODD.
  
 
-a=int(input("enter any number from  1 to 100:"))
-if(a % 2 == 0):
-    print("the number is even")
+#a=int(input("enter any number from  1 to 100:"))
+#if(a % 2 == 0):
+#    print("the number is even")
+#else:
+#    print("the number is odd")
+#Q2-WAP TO  FIND THE GREATEST OF 3 NUMBER ENTERED BY USER.
+
+
+#a=int(input("Enter any number here:"))
+#b=int(input("Enter any number here:"))
+#c=int(input("Enter any number here:"))
+
+#if(a > b and a > c):
+#    print(a," is greater")
+#elif(b > a and b > c):
+#    print(b," is greater")
+#else:
+#    print(c,"is greater") 
+
+
+#Q3- WAP to check whether the number given by user is multiple of 7 or not
+
+#x=int(input("Enter any number:"))
+#if(x % 7 ==0):
+#    print(x,"number is multiple of 7")
+#else:
+#   print(x,"number is not multiple of 7")
+
+
+#Q4- WAP to check if a character entered by the user is a vowel or a consonant.
+
+#ch=input("Enter any character:")
+#if ch in 'aeiou':
+#    print(ch, "the character is vowel")
+#else:
+#    print(ch,"the character entered is consonant")
+
+
+#Q5 -WAP to check if the given temperature in Celsius is cold, warm, or hot.
+ 
+temp=float(input("Enter the temperature in celsius:"))
+if(temp  < 10 ):
+    print("its cold")
+elif(10 <= temp < 25):
+    print("its warm ")
 else:
-    print("the number is odd")
+    print("its hot") 
