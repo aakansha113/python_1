a04bce6b0a144cd58859b19be6c8a15b69451b67
aakansha <aakanshahujare@gmail.com>
2025-03-04 06:14:48 +0000

    add

---
+++ python/statement/pratice.py
@@ -0,0 +1,8 @@
+#WAP  TO CHECk WHETHER THE GIVEN NUMBER ENTER BY USER IS EVEN OR ODD.
+ 
+
+a=int(input("enter any number from  1 to 100:"))
+if(a % 2 == 0):
+    print("the number is even")
+else:
+    print("the number is odd")
