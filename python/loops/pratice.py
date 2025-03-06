17585f3e96133ca3b8e898f9730bc64994428d8a
aakansha113 <155349555+aakansha113@users.noreply.github.com>
2025-03-06 11:41:43 +0530

    pratice.py

--- python/loops/pratice.py
+++ python/loops/pratice.py
@@ -1,16 +1,16 @@
 #WAP to print number from 1 to 100
-#i=1
-#while i <=100:
-#    print(i)
-#    i +=1
+i=1
+while i <=100:
+   print(i)
+    i +=1
 
 
 #WAP to print number from 100 to 1
 
-#i =100 
-#while i >=1:
-#    print(i)
-#    i -=1
+i =100 
+while i >=1:
+    print(i)
+    i -=1
 
 #WAP to print multiple from  the user.
 n=int(input("enter any number"))
